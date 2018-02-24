b5bc6e8db351374b40c3c866f536e2d5e1a92fff
windgaucho <luissoto9@gmail.com>
2018-02-24 19:19:47 -0300

    trabajando en rubros

--- src/components/administracion/rubros/Rubro.vue
+++ src/components/administracion/rubros/Rubro.vue
@@ -1,29 +1,43 @@
 <template>
   <section>
     <h1 class="title">Nuevo Rubro</h1>
-    <form v-on:submit.prevent="createRubro">
+    <div>
       <b-field label="Descripción">
-        <b-input v-model="descripcion" id="descripcion"></b-input>
+        <b-input v-model="rubro.descripcion" id="descripcion"></b-input>
       </b-field>
       <b-field label="Tipo">
-        <b-select placeholder="Seleccione el tipo" v-model="tipo">
+        <b-select placeholder="Seleccione el tipo" v-model="rubro.tipo">
           <option value="INGRESO">Ingreso</option>
           <option value="EGRESO">Egreso</option>
         </b-select>
       </b-field>
       <b-field label="Grupo">
-        <b-input v-model="grupo" id="grupo"></b-input>
+        <b-input v-model="rubro.grupo" id="grupo"></b-input>
       </b-field>
       <b-field grouped>
-        <button class="control button is-danger">Guardar</button>
+        <button class="control button is-danger" @click="submit">Guardar</button>
         <button class="control button is-dark">Cancelar</button>
       </b-field>
-    </form>
+    </div>
   </section>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        rubro: {
+          descripcion: '',
+          tipo: '',
+          grupo: '',
+        }
+      };
+    },
+
+    methods: {
+      submit() {
+        console.log('rubro', this.rubro);
+      },
+    }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
 import Buefy from 'buefy';
 import 'buefy/lib/buefy.css';
 
 import App from './App.vue'
 import { routes } from './routes';
 
+Vue.use(VueResource);
 Vue.use(Buefy);
 Vue.use(VueRouter);
 
